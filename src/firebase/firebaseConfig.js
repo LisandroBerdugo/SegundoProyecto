82b55d9f91ae7b9f8f2eab14674dd4e9418fbcc7
Lisandro Berdugo <lisanro1995@gmail.com>
2026-05-17 01:28:48 -0600

    Primer commit proyecto comunidad eventos

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJYtSgSKAwtBWnxpQZfD-gRmEUpgt-nDQ",
+  authDomain: "comunidad-eventos.firebaseapp.com",
+  projectId: "comunidad-eventos",
+  storageBucket: "comunidad-eventos.firebasestorage.app",
+  messagingSenderId: "642610408457",
+  appId: "1:642610408457:web:dc762389aadee723db5cd8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import HomeUserScreen from '../screens/HomeUserScreen';
+import HomeAdminScreen from '../screens/HomeAdminScreen';
+import EventsScreen from '../screens/EventsScreen';
+import CreateEventScreen from '../screens/CreateEventScreen';
+import EventDetailScreen from '../screens/EventDetailScreen';
+import ReservationSummaryScreen from '../screens/ReservationSummaryScreen';
+import QrScreen from '../screens/QrScreen';
+import ScanQrScreen from '../screens/ScanQrScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Registro" component={RegisterScreen} />
+        <Stack.Screen name="InicioUsuario" component={HomeUserScreen} />
+        <Stack.Screen name="InicioAdmin" component={HomeAdminScreen} />
+        <Stack.Screen name="Eventos" component={EventsScreen} />
+        <Stack.Screen name="CrearEvento" component={CreateEventScreen} />
+        <Stack.Screen name="DetalleEvento" component={EventDetailScreen} />
+        <Stack.Screen name="ResumenReserva" component={ReservationSummaryScreen} />
+        <Stack.Screen name="CodigoQR" component={QrScreen} />
+        <Stack.Screen name="EscanearQR" component={ScanQrScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
